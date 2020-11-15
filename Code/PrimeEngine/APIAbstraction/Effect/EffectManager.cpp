723d2e4ea892e49a4fd12a402de6c0719b3dc997
Jingtao <jingtaoh@usc.edu>
2020-11-15 03:52:44 -0800

    Fix single material

--- Code/PrimeEngine/APIAbstraction/Effect/EffectManager.cpp
+++ Code/PrimeEngine/APIAbstraction/Effect/EffectManager.cpp
@@ -25,11 +25,16 @@
 #include "PrimeEngine/Scene/CameraSceneNode.h"
 #include "PrimeEngine/Scene/CameraManager.h"
 #include "PrimeEngine/APIAbstraction/GPUMaterial/GPUMaterialSet.h"
+#include "PrimeEngine/Scene/SH_DRAW.h"
+#include "PrimeEngine/Scene/SkeletonInstance.h"
+#include "PrimeEngine/Scene/DefaultAnimationSM.h"
 
 // Sibling/Children includes
 #include "EffectManager.h"
 #include "Effect.h"
 
+extern int g_disableSkinRender;
+extern int g_iDebugBoneSegment;
 
 namespace PE {
 
@@ -511,6 +516,7 @@ void EffectManager::drawMirrorPass()
 }
 void EffectManager::drawReflectionPass()
 {
+
 	Effect& curEffect = *m_hReflectionEffect.getObject<Effect>();
 	if (!curEffect.m_isReady)
 		return;
@@ -523,100 +529,365 @@ void EffectManager::drawReflectionPass()
 	if (!pMesh)
 		return;
 
-	// index buf
-	IndexBufferGPU* pibGPU = pMesh->m_hIndexBufferGPU.getObject<IndexBufferGPU>();
-	pibGPU->setAsCurrent();
+	// index buffer
+	Handle hIBuf = pMesh->m_hIndexBufferGPU;
+	IndexBufferGPU* pibGPU = hIBuf.getObject<IndexBufferGPU>();
+	//pibGPU->setAsCurrent();
 
 	// vertex buf
 	VertexBufferGPU* pvbGPU;
 	pvbGPU = pMesh->m_vertexBuffersGPUHs[0].getObject<VertexBufferGPU>();
-	pvbGPU->setAsCurrent(&curEffect);
-
-	// effect
+	//pvbGPU->setAsCurrent(&curEffect);
 	
-	curEffect.setCurrent(pvbGPU);
-
-	// texture
-	GPUMaterialSet* pGpuMatSet = pMesh->m_hMaterialSetGPU.getObject<GPUMaterialSet>();
-	PE::Handle hSV("SA_Bind_Resource", sizeof(SA_Bind_Resource));
-	SA_Bind_Resource* pSetTextureAction = new(hSV) SA_Bind_Resource(*m_pContext, m_arena);
-	for (int i = 0; i < 1/*pGpuMatSet->m_materials.m_size*/; i++)
+	// Check for vertex buffer(s)
+	Handle hVertexBuffersGPU[4]; // list of bufers to pass to GPU
+	Vector4 vbufWeights;
+	int numVBufs = pMesh->m_vertexBuffersGPUHs.m_size;
+	assert(numVBufs < 4);
+	for (int ivbuf = 0; ivbuf < numVBufs; ivbuf++)
 	{
-		GPUMaterial& curMat = pGpuMatSet->m_materials[i];
-		for (PrimitiveTypes::UInt32 itex = 0; itex < curMat.m_textures.m_size; itex++)
+		hVertexBuffersGPU[ivbuf] = pMesh->m_vertexBuffersGPUHs[ivbuf];
+		vbufWeights.m_values[ivbuf] = hVertexBuffersGPU[ivbuf].getObject<VertexBufferGPU>()->m_weight;
+	}
+
+	if (numVBufs > 1)
+	{
+		for (int ivbuf = numVBufs; ivbuf < 4; ivbuf++)
 		{
-
-			
-			// create object referenced by Handle in DrawList
-			// this handle will be released on end of draw call, os no need to worry about realeasing this object
-			
-
-			//SA_Bind_Resource *pSetTextureAction;
-			TextureGPU& curTex = *curMat.m_textures[itex].getObject<TextureGPU>();
-			pSetTextureAction->set(
-				DIFFUSE_TEXTURE_2D_SAMPLER_SLOT,
-				curTex.m_samplerState,
-#if APIABSTRACTION_D3D9
-				curTex.m_pTexture,
-#elif APIABSTRACTION_D3D11
-				curTex.m_pShaderResourceView,
-#elif APIABSTRACTION_OGL
-				curTex.m_texture,
-#elif PE_PLAT_IS_PSVITA
-				curTex.m_texture,
-#endif
-				curTex.m_name
-			);
-			pSetTextureAction->bindToPipeline(&curEffect);
+			hVertexBuffersGPU[ivbuf] = hVertexBuffersGPU[0];
+			vbufWeights.m_values[ivbuf] = vbufWeights.m_values[0];
 		}
+		numVBufs = 4; // blend shape shader works with 4 shapes. so we extend whatever slots with base
+	}
+
+	PE::Handle hSA("SA_Bind_Resource", sizeof(SA_Bind_Resource));
+	PE::Handle hSPMCSA("SetPerMaterialConstantsShaderAction", sizeof(SetPerMaterialConstantsShaderAction));
+	PE::Handle hSPOCSA("SetPerObjectConstantsShaderAction", sizeof(SetPerObjectConstantsShaderAction));
+
+	// Check for material set
+	if (!pMesh->m_hMaterialSetGPU.isValid())
+		return;
+
+	// draw all pixel ranges with different materials
+	PrimitiveTypes::UInt32 numRanges = MeshHelpers::getNumberOfRangeCalls(pibGPU);
+
+	for (PrimitiveTypes::UInt32 iRange = 0; iRange < numRanges; iRange++)
+	{
+		// we might have several passes (several effects) so we need to check which effect list to use
+		PEStaticVector<PE::Handle, 4>* pEffectsForRange = NULL;
+
+		IndexBufferGPU* pibGPU = hIBuf.getObject<IndexBufferGPU>();
+
+		IndexRange& ir = pibGPU->m_indexRanges[iRange];
+		bool hasJointSegments = ir.m_boneSegments.m_size > 0;
+
+
+		{
+			//if instance count is 1, then regular effects are preferred
+			//if instance count is > 1 then, if instance effect is available use it, otherwise use normal effect instanceCount times
+
+			//first of all, return if have no appropriate effects
+			if (pMesh->m_effects[iRange].m_size == 0 && pMesh->m_instanceEffects[iRange].m_size == 0) // this effect does not render in normal passes
+			{
+				return;
+			}
+
+			pEffectsForRange = &pMesh->m_effects[iRange];
+
+			// check that have normal (non-instance) effects that are all valid
+			// try to use effect for instances, otherwise will have to render objects one at a time
+			bool haveEffect = pMesh->m_effects[iRange].m_size > 0;
+			for (unsigned int iPass = 0; iPass < pMesh->m_effects[iRange].m_size; ++iPass)
+			{
+				if (!pMesh->m_effects[iRange][iPass].isValid())
+				{
+					haveEffect = false;
+					break;
+				}
+			}
+			PEASSERT(haveEffect, "Can't find an effect to render asset with. Note there is only one instance of the asset " \
+				"and default behavior is to use no instance version of effect. Potentially we could add code to use instanced version of effect even if there is only one instance rendered");
+			if (!haveEffect)
+			{
+				// some are not valid or don't have effects at all
+				return;
+			}
+			
+		}
+
+		CameraSceneNode* pcam = CameraManager::Instance()->getActiveCamera()->getCamSceneNode();
+		Matrix4x4 evtProjectionViewWorldMatrix = pcam->m_viewToProjectedTransform * pcam->m_worldToViewTransform;
+
+		GPUMaterialSet* pGpuMatSet = pMesh->m_hMaterialSetGPU.getObject<GPUMaterialSet>();
+		GPUMaterial& curMat = pGpuMatSet->m_materials[iRange];
+
+		for (PrimitiveTypes::UInt32 iEffect = 0; iEffect < pEffectsForRange->m_size; ++iEffect)
+		{
+			Handle hEffect = (*pEffectsForRange)[iEffect];
+			Effect* pEffect = hEffect.getObject<Effect>();
+
+			int numRenderGroups = pMesh->m_numVisibleInstances;
+
+			// each render group is group of instances that can be submitted at once
+			// when there is no instancing effect
+			int iSrcInstance = 0; // tracks next instance index to be submitted. we might skip meshes based on culling
+			// so this value might not be equal to iRender group even for non-instanced case
+			for (int iRenderGroup = 0; iRenderGroup < numRenderGroups; ++iRenderGroup)
+			{
+				Handle hLodIB = hIBuf;
+				IndexBufferGPU* pLodibGPU = pibGPU;
+				Handle hLODVB[4];
+				PEASSERT(numVBufs < 4, "Too many vbs");
+
+				PE::Handle* pHVBs = &hVertexBuffersGPU[0];
+
+				for (int ivbuf = 0; ivbuf < numVBufs; ivbuf++)
+				{
+					hLODVB[ivbuf] = pHVBs[ivbuf];
+				}
+
+				PrimitiveTypes::UInt32 numJointSegments = hasJointSegments ? ir.m_boneSegments.m_size : 1;
+
+				if (g_disableSkinRender && hasJointSegments)
+					numJointSegments = 0;
+
+				int iSrcInstanceInBoneSegment = 0;
+				for (PrimitiveTypes::UInt32 _iBoneSegment = 0; _iBoneSegment < numJointSegments; _iBoneSegment++)
+				{
+					PrimitiveTypes::UInt32 iBoneSegment = _iBoneSegment;
+					if (g_iDebugBoneSegment >= 0 && g_iDebugBoneSegment < numJointSegments)
+					{
+						iBoneSegment = g_iDebugBoneSegment;
+						if (_iBoneSegment) break;
+					}
+
+					iSrcInstanceInBoneSegment = iSrcInstance; // reset instance id for each bone segment since we want to process same instances
+					while (pMesh->m_instances[iSrcInstanceInBoneSegment].getObject<MeshInstance>()->m_culledOut)
+						++iSrcInstanceInBoneSegment;
+
+					if (hLodIB.isValid())
+						pLodibGPU->setAsCurrent();
+						
+					
+
+					if (numVBufs == 1)
+					{
+						VertexBufferGPU* pCurVertBuf = hLODVB[0].getObject<VertexBufferGPU>();
+						pCurVertBuf->setAsCurrent(pEffect);
+					}
+
+
+					for (int ivbuf = 0; ivbuf < numVBufs; ivbuf++)
+					{
+						pvbGPU->setAsCurrent(pEffect);
+					}
+
+					pEffect->setCurrent(pvbGPU);
+
+					// shader actions
+					for (PrimitiveTypes::UInt32 itex = 0; itex < curMat.m_textures.m_size; itex++)
+					{
+						Handle hCurTex = curMat.m_textures[itex];
+						TextureGPU& curTex = *hCurTex.getObject<TextureGPU>();
+
+						SA_Bind_Resource* pSetTextureAction = new(hSA) SA_Bind_Resource(*m_pContext, m_arena);
+
+						pSetTextureAction->set(
+							DIFFUSE_TEXTURE_2D_SAMPLER_SLOT,
+							curTex.m_samplerState,
+#if APIABSTRACTION_D3D9
+							curTex.m_pTexture,
+#elif APIABSTRACTION_D3D11
+							curTex.m_pShaderResourceView,
+#elif APIABSTRACTION_OGL
+							curTex.m_texture,
+#elif PE_PLAT_IS_PSVITA
+							curTex.m_texture,
+#endif
+							curTex.m_name
+						);
+						pSetTextureAction->bindToPipeline(pEffect);
+
+						// set constant buffer
+						// this handle will be released on end of draw call
+						SetPerMaterialConstantsShaderAction* pSV = new(hSPMCSA) SetPerMaterialConstantsShaderAction();
+
+						pSV->m_data.m_diffuse = curMat.m_diffuse;
+						pSV->m_data.gxyzVSpecular_w.asVector3Ref() = curMat.m_specular;
+						pSV->m_data.gxyzVEmissive_wVShininess.asVector3Ref() = curMat.m_emissive;
+						pSV->m_data.gxyzVEmissive_wVShininess.m_w = curMat.m_shininess;
+						pSV->m_data.m_xHasNrm_yHasSpec_zHasGlow_wHasDiff = curMat.m_xHasNrm_yHasSpec_zHasGlow_wHasDiff;
+						pSV->bindToPipeline(pEffect);
+
+					}
+
+					// SetPerObjectConstantsShaderAction
+					{
+						MeshInstance* pInst = pMesh->m_instances[0].getObject<MeshInstance>();
+
+						SetPerObjectConstantsShaderAction* psvPerObject = new(hSPOCSA) SetPerObjectConstantsShaderAction();
+
+						memset(&psvPerObject->m_data, 0, sizeof(SetPerObjectConstantsShaderAction::Data));
+
+
+						Handle hParentSN = pInst->getFirstParentByType<SceneNode>();
+						SkeletonInstance* pParentSkelInstance = NULL;
+						if (!hParentSN.isValid())
+						{
+							// allow skeleton to be in chain
+							if (pParentSkelInstance = pInst->getFirstParentByTypePtr<SkeletonInstance>())
+							{
+								hParentSN = pParentSkelInstance->getFirstParentByType<SceneNode>();
+							}
+						}
+						PEASSERT(hParentSN.isValid(), "Each instance must have a scene node parent");
+
+						Matrix4x4& m_worldTransform = hParentSN.getObject<SceneNode>()->m_worldTransform;
+
+						Matrix4x4 worldMatrix = hParentSN.getObject<SceneNode>()->m_worldTransform;
+
+						Vector3 worldPos;
+						worldPos = worldMatrix.getPos();
+						worldMatrix.setPos(Vector3(worldPos.m_x, worldPos.m_y, -worldPos.m_z));
+
+						psvPerObject->m_data.gWVP = evtProjectionViewWorldMatrix * worldMatrix; // these values are only used by non-instance version
+						psvPerObject->m_data.gWVPInverse = psvPerObject->m_data.gWVP.inverse(); // these values are only used by non-instance version
+
+						psvPerObject->m_data.gW = worldMatrix;  // these values are only used by non-instance version
+
+						// Set blend weights
+						if (numVBufs > 1)
+						{
+							psvPerObject->m_data.gVertexBufferWeights = vbufWeights;
+						}
+
+						if (hasJointSegments)
+						{
+							DefaultAnimationSM* pAnimSM = pParentSkelInstance ? pParentSkelInstance->getFirstComponent<DefaultAnimationSM>() : NULL;
+
+							if (pAnimSM)
+							{
+								psvPerObject->m_useBones = true;
+
+								IndexRange::BoneSegment& bs = ir.m_boneSegments[iBoneSegment];
+								Matrix4x4* curPalette = pAnimSM->m_curPalette.getFirstPtr();
+								for (int ibone = 0; ibone < (int)(bs.m_boneSegmentBones.m_size > PE_MAX_BONE_COUNT_IN_DRAW_CALL ? PE_MAX_BONE_COUNT_IN_DRAW_CALL : bs.m_boneSegmentBones.m_size); ibone++)
+								{
+									if (pAnimSM->m_curPalette.m_size > 0)
+									{
+										memcpy(&psvPerObject->m_data.gJoints[ibone], &(curPalette[bs.m_boneSegmentBones[ibone]]), sizeof(Matrix4x4));
+									}
+								}
+							}
+						} // end if has bone segments
+						psvPerObject->bindToPipeline(pEffect);
+					} 
+
+					
+				}	// for each bone segment (if no bone segment, just run the code once, for a non skinned mesh)
+
+				iSrcInstance = iSrcInstanceInBoneSegment + 1;
+				//pLodibGPU->draw(1, 0);
+				pLodibGPU->draw(iRange, 1, 0);
+				//pSetTextureAction->unbindFromPipeline(&curEffect);
+
+			}
+		}	// loop through all effects for this iRange
+
 	}
 	
 
-	//TextureGPU* t = m_hGlowTargetTextureGPU.getObject<TextureGPU>();
-	//PE::SA_Bind_Resource setTextureAction(*m_pContext, m_arena, DIFFUSE_TEXTURE_2D_SAMPLER_SLOT, t->m_samplerState, API_CHOOSE_DX11_DX9_OGL(t->m_pShaderResourceView, t->m_pTexture, t->m_texture));
-	//setTextureAction.bindToPipeline(&curEffect);
+	hSA.release();
+	hSPMCSA.release();
+	hSPOCSA.release();
 
-	// Per Object
-
-	PE::SetPerObjectConstantsShaderAction objSa;
-
-	MeshInstance* pInst = pMesh->m_instances[0].getObject<MeshInstance>();
-
-	//Handle& hsvPerObject = Handle("RAW_DATA", sizeof(SetPerObjectConstantsShaderAction));
-	//SetPerObjectConstantsShaderAction* psvPerObject = new(hsvPerObject) SetPerObjectConstantsShaderAction();
-
-	//memset(&psvPerObject->m_data, 0, sizeof(SetPerObjectConstantsShaderAction::Data));
-
-	Handle hParentSN = pInst->getFirstParentByType<SceneNode>();
-
-	Matrix4x4& m_worldTransform = hParentSN.getObject<SceneNode>()->m_worldTransform;
-
-	Matrix4x4 worldMatrix = hParentSN.getObject<SceneNode>()->m_worldTransform;
-
-	CameraSceneNode* pcam = CameraManager::Instance()->getActiveCamera()->getCamSceneNode();
-
-	Matrix4x4 evtProjectionViewWorldMatrix = pcam->m_viewToProjectedTransform * pcam->m_worldToViewTransform;
-
-	Vector3 worldPos;
-	worldPos = worldMatrix.getPos();
-	worldMatrix.setPos(Vector3(worldPos.m_x, worldPos.m_y, -worldPos.m_z));
-
-	objSa.m_data.gWVP = evtProjectionViewWorldMatrix * worldMatrix; // these values are only used by non-instance version
-	objSa.m_data.gWVPInverse = objSa.m_data.gWVP.inverse(); // these values are only used by non-instance version
-
-	objSa.m_data.gW = worldMatrix;  // these values are only used by non-instance version
-	objSa.bindToPipeline(&curEffect);
-
-	//SetPerMaterialConstantsShaderAction* pSV = new(hSV) SetPerMaterialConstantsShaderAction();
-
-	//pSV->m_data.m_diffuse = m_diffuse;
-	//pSV->m_data.gxyzVSpecular_w.asVector3Ref() = m_specular;
-	//pSV->m_data.gxyzVEmissive_wVShininess.asVector3Ref() = m_emissive;
-	//pSV->m_data.gxyzVEmissive_wVShininess.m_w = m_shininess;
-	//pSV->m_data.m_xHasNrm_yHasSpec_zHasGlow_wHasDiff = m_xHasNrm_yHasSpec_zHasGlow_wHasDiff;
-	pibGPU->draw(1, 0);
-
-	pSetTextureAction->unbindFromPipeline(&curEffect);
+//	// effect
+//	
+//	curEffect.setCurrent(pvbGPU);
+//
+//	// texture
+//	GPUMaterialSet* pGpuMatSet = pMesh->m_hMaterialSetGPU.getObject<GPUMaterialSet>();
+//	PE::Handle hSV("SA_Bind_Resource", sizeof(SA_Bind_Resource));
+//	SA_Bind_Resource* pSetTextureAction = new(hSV) SA_Bind_Resource(*m_pContext, m_arena);
+//	for (int i = 0; i < pGpuMatSet->m_materials.m_size; i++)
+//	{
+//		GPUMaterial& curMat = pGpuMatSet->m_materials[i];
+//		for (PrimitiveTypes::UInt32 itex = 0; itex < curMat.m_textures.m_size; itex++)
+//		{
+//
+//			
+//			// create object referenced by Handle in DrawList
+//			// this handle will be released on end of draw call, os no need to worry about realeasing this object
+//			
+//
+//			//SA_Bind_Resource *pSetTextureAction;
+//			TextureGPU& curTex = *curMat.m_textures[itex].getObject<TextureGPU>();
+//			pSetTextureAction->set(
+//				DIFFUSE_TEXTURE_2D_SAMPLER_SLOT,
+//				curTex.m_samplerState,
+//#if APIABSTRACTION_D3D9
+//				curTex.m_pTexture,
+//#elif APIABSTRACTION_D3D11
+//				curTex.m_pShaderResourceView,
+//#elif APIABSTRACTION_OGL
+//				curTex.m_texture,
+//#elif PE_PLAT_IS_PSVITA
+//				curTex.m_texture,
+//#endif
+//				curTex.m_name
+//			);
+//			pSetTextureAction->bindToPipeline(&curEffect);
+//		}
+//	}
+//	
+//
+//	//TextureGPU* t = m_hGlowTargetTextureGPU.getObject<TextureGPU>();
+//	//PE::SA_Bind_Resource setTextureAction(*m_pContext, m_arena, DIFFUSE_TEXTURE_2D_SAMPLER_SLOT, t->m_samplerState, API_CHOOSE_DX11_DX9_OGL(t->m_pShaderResourceView, t->m_pTexture, t->m_texture));
+//	//setTextureAction.bindToPipeline(&curEffect);
+//
+//	// Per Object
+//
+//	PE::SetPerObjectConstantsShaderAction objSa;
+//
+//	MeshInstance* pInst = pMesh->m_instances[0].getObject<MeshInstance>();
+//
+//	//Handle& hsvPerObject = Handle("RAW_DATA", sizeof(SetPerObjectConstantsShaderAction));
+//	//SetPerObjectConstantsShaderAction* psvPerObject = new(hsvPerObject) SetPerObjectConstantsShaderAction();
+//
+//	//memset(&psvPerObject->m_data, 0, sizeof(SetPerObjectConstantsShaderAction::Data));
+//
+//	Handle hParentSN = pInst->getFirstParentByType<SceneNode>();
+//
+//	Matrix4x4& m_worldTransform = hParentSN.getObject<SceneNode>()->m_worldTransform;
+//
+//	Matrix4x4 worldMatrix = hParentSN.getObject<SceneNode>()->m_worldTransform;
+//
+//	CameraSceneNode* pcam = CameraManager::Instance()->getActiveCamera()->getCamSceneNode();
+//
+//	Matrix4x4 evtProjectionViewWorldMatrix = pcam->m_viewToProjectedTransform * pcam->m_worldToViewTransform;
+//
+//	Vector3 worldPos;
+//	worldPos = worldMatrix.getPos();
+//	worldMatrix.setPos(Vector3(worldPos.m_x, worldPos.m_y, -worldPos.m_z));
+//
+//	objSa.m_data.gWVP = evtProjectionViewWorldMatrix * worldMatrix; // these values are only used by non-instance version
+//	objSa.m_data.gWVPInverse = objSa.m_data.gWVP.inverse(); // these values are only used by non-instance version
+//
+//	objSa.m_data.gW = worldMatrix;  // these values are only used by non-instance version
+//	objSa.bindToPipeline(&curEffect);
+//
+//	//SetPerMaterialConstantsShaderAction* pSV = new(hSV) SetPerMaterialConstantsShaderAction();
+//
+//	//pSV->m_data.m_diffuse = m_diffuse;
+//	//pSV->m_data.gxyzVSpecular_w.asVector3Ref() = m_specular;
+//	//pSV->m_data.gxyzVEmissive_wVShininess.asVector3Ref() = m_emissive;
+//	//pSV->m_data.gxyzVEmissive_wVShininess.m_w = m_shininess;
+//	//pSV->m_data.m_xHasNrm_yHasSpec_zHasGlow_wHasDiff = m_xHasNrm_yHasSpec_zHasGlow_wHasDiff;
+//	pibGPU->draw(1, 0);
+//
+//	pSetTextureAction->unbindFromPipeline(&curEffect);
+//
+//	hSV.release();
 	
 }
 
